82e569cac7538e8728f12b8b335ba206434eacd6
Cykotic <61665278+Cykotic@users.noreply.github.com>
2021-05-20 14:34:30 -0400

    Delete reconDB.js

--- src/model/reconDB.js
+++
@@ -1,8 +0,0 @@
-const { reconDB } = require('reconlx')
-const client = require('../index')
-const { mongo } = require("../config.json")
-const db = new reconDB(client, {
-    uri: `${mongo}`,
-});
-
-module.exports = db;
