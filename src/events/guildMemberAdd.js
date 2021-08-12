5159c7ed486d42bc9bd5121091fe9d1449f2b9f2
Cykotic <61665278+Cykotic@users.noreply.github.com>
2021-08-11 23:08:05 -0400

    Delete src directory

--- src/events/guildMemberAdd.js
+++
@@ -1,9 +0,0 @@
-const client = require('../index')  
-const db = require('../model/reconDB')
-
-client.on('guildMemberAdd', async (member) => {
-    const check = await db.has(`autorole-${member.guild.id}`);
-    if(check === true) {
-        member.roles.add(await db.get(`autorole-${member.guild.id}`))
-    }
-})
